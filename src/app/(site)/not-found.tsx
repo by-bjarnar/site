2f5b7e7c7dd13a4743a5051e68824e7633549cf8
Henry Bugajski <henry.bug.dev@gmail.com>
2025-04-05 22:02:05 -0400

    chore: run prettier

--- src/app/(site)/not-found.tsx
+++ src/app/(site)/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <section>
-      <h1 className="xs:text-5xl mb-4 text-4xl">Page not found</h1>
+      <h1 className="mb-4 text-4xl xs:text-5xl">Page not found</h1>
       <p>We couldn&apos;t find the page you were looking for.</p>
     </section>
   );
